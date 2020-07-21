85d6e68d59cd8d687239e7327e12b4a40eec2b95
Jenkin Tang <jenkincs@163.com>
2020-07-21 23:09:36 +0800

    pos-machine-2020-7-20-13-59-35-216 jenkin-Tang feature:implement a function called printReceipt

--- README.md
+++ README.md
@@ -1,72 +1,8 @@
-# Requirement
-A cash register (POS) system is used in the store for settlement of the store. This cashier will settle and print the receipt (Receipt) according to the item(Item) in the customer's shopping cart (Cart) at the time of settlement.
-
-We need to implement a function called printReceipt, which can input the data of the specified format as a parameter and then output the text of the receipt in the browser console.
-
-This time, the input will be an array of barcodes (string). For example:
-```javascript
-[
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000000',
-  'ITEM000001',
-  'ITEM000001',
-  'ITEM000004'
-]
-```
-
-Then the output should be 
-```
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************
-```
-
-Suppose that our database is as follows:
-```javascript
-[
-   {
-      barcode: 'ITEM000000',
-      name: 'Coca-Cola',
-      price: 3
-    },
-    {
-      barcode: 'ITEM000001',
-      name: 'Sprite',
-      price: 3
-    },
-    {
-      barcode: 'ITEM000002',
-      name: 'Apple',
-      price: 5
-    },
-    {
-      barcode: 'ITEM000003',
-      name: 'Litchi',
-      price: 15
-    },
-    {
-      barcode: 'ITEM000004',
-      name: 'Battery',
-      price: 2
-    },
-    {
-      barcode: 'ITEM000005',
-      name: 'Instant Noodles',
-      price: 4
-    }
-]
-```
-
-# Principal
-
-1. Please draw context diagram.
-2. Please declare all the methods according to your diagram.
-3. Please implement the function according to the context diagram
-4. Please repeat step 3 until all functions are implemented.
+## 分解任务
+1. 获取商品和对应的数量
+2. 获取每种商品的单价
+3. 获取每种商品的总价钱
+4. 获取所有商品的总价钱
+5. 获取每种商品的发票信息
+6. 格式化发票信息
+7. 打印发票信息
--- main.js
+++ main.js
@@ -1,15 +1,178 @@
 function printReceipt(barcodes) {
-    console.log(`
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************`)
+    const databaseRecords = [
+	   {
+	      barcode: 'ITEM000000',
+	      name: 'Coca-Cola',
+	      price: 3
+	    },
+	    {
+	      barcode: 'ITEM000001',
+	      name: 'Sprite',
+	      price: 3
+	    },
+	    {
+	      barcode: 'ITEM000002',
+	      name: 'Apple',
+	      price: 5
+	    },
+	    {
+	      barcode: 'ITEM000003',
+	      name: 'Litchi',
+	      price: 15
+	    },
+	    {
+	      barcode: 'ITEM000004',
+	      name: 'Battery',
+	      price: 2
+	    },
+	    {
+	      barcode: 'ITEM000005',
+	      name: 'Instant Noodles',
+	      price: 4
+	    }
+	];
+
+	//printReceipt(barcodes);
+	//getNoRepectBarcodeArray(barcodes);
+	var cartItems = getItemCountByBarcode(barcodes);
+	var cartItemDetails = getItemInfoByBarcode(cartItems,databaseRecords);
+	var itemReceipts = getItemSubtotalPrice(cartItemDetails);
+	var totalPrice = getAllItemTotalPrice(itemReceipts);
+	var receiptInfo = getReceiptInfo(itemReceipts,totalPrice);
+    console.log(receiptInfo);
+}
+
+function getNoRepectBarcodeArray(array){
+    array.sort(); 
+    var re=[array[0]];
+    for(var i = 1; i < array.length; i++){
+   		if( array[i] !== re[re.length-1])
+   		{
+     		re.push(array[i]);
+   		}
+    }
+	//console.log(re);
+    return re;
+}
+
+function getItemCountByBarcode(barcodes){
+	var cartItems = [];
+	var noRepectBarcodeArray = getNoRepectBarcodeArray(barcodes);
+	for (var i = 0; i < noRepectBarcodeArray.length; i++) {
+		var count = 0;
+		for (var j = barcodes.length - 1; j >= 0; j--) {
+			if(barcodes[j]==noRepectBarcodeArray[i]){
+				count++;
+			}
+		}
+		var cartItem = {barcode:noRepectBarcodeArray[i],quantity:count};
+		cartItems.push(cartItem);
+	}
+	return cartItems;
+	//console.log(cartItems);
+}
+
+function getItemInfoByBarcode(cartItems,databaseRecords){
+	for (var i = 0; i < cartItems.length; i++) {
+		for (var j = databaseRecords.length - 1; j >= 0; j--) {
+			if(databaseRecords[j].barcode == cartItems[i].barcode){
+				cartItems[i].price = databaseRecords[j].price;
+				cartItems[i].name = databaseRecords[j].name;
+			}
+		}
+	}
+	//console.log(cartItems);
+	return cartItems;
+}
+
+function getItemSubtotalPrice(cartItemDetails){
+	for (var i = 0; i < cartItemDetails.length; i++) {
+		cartItemDetails[i].subtotal = cartItemDetails[i].quantity * cartItemDetails[i].price;
+	}
+
+	//console.log(cartItemDetails);
+	return cartItemDetails;
+}
+
+function getAllItemTotalPrice(itemReceipts){
+	var totalPrice = 0;
+	for (var i = 0; i < itemReceipts.length; i++) {
+		totalPrice += itemReceipts[i].subtotal;
+	}
+	//console.log(totalPrice);
+	return totalPrice;
+}
+
+function getReceiptInfo(itemReceipts,totalPrice){
+	var receiptInfo = "\n***<store earning no money>Receipt ***\n";
+	var itemReceiptInfo = "";
+	for (var i = 0; i < itemReceipts.length; i++) {
+		itemReceiptInfo += "Name: " + itemReceipts[i].name + ", Quantity: "+ itemReceipts[i].quantity + ", Unit price: " + 
+		itemReceipts[i].price + " (yuan), Subtotal: " + itemReceipts[i].subtotal + " (yuan)\n";
+	}
+	receiptInfo += itemReceiptInfo;
+	receiptInfo +=  "----------------------\n";
+	receiptInfo +=  "Total: " + totalPrice + " (yuan)\n";
+	receiptInfo += "**********************";
+
+	//console.log(receiptInfo);
+	return receiptInfo;
 }
 
 
 module.exports = {
     printReceipt
 };
+
+
+/*const barcodes = [
+                'ITEM000000',
+                'ITEM000000',
+                'ITEM000000',
+                'ITEM000000',
+                'ITEM000000',
+                'ITEM000001',
+                'ITEM000001',
+                'ITEM000004'
+            ];
+
+const databaseRecords = [
+   {
+      barcode: 'ITEM000000',
+      name: 'Coca-Cola',
+      price: 3
+    },
+    {
+      barcode: 'ITEM000001',
+      name: 'Sprite',
+      price: 3
+    },
+    {
+      barcode: 'ITEM000002',
+      name: 'Apple',
+      price: 5
+    },
+    {
+      barcode: 'ITEM000003',
+      name: 'Litchi',
+      price: 15
+    },
+    {
+      barcode: 'ITEM000004',
+      name: 'Battery',
+      price: 2
+    },
+    {
+      barcode: 'ITEM000005',
+      name: 'Instant Noodles',
+      price: 4
+    }
+];
+
+//printReceipt(barcodes);
+//getNoRepectBarcodeArray(barcodes);
+var cartItems = getItemCountByBarcode(barcodes);
+var cartItemDetails = getItemInfoByBarcode(cartItems,databaseRecords);
+var itemReceipts = getItemSubtotalPrice(cartItemDetails);
+var totalPrice = getAllItemTotalPrice(itemReceipts);
+var receiptInfo = getReceiptInfo(itemReceipts,totalPrice);*/
